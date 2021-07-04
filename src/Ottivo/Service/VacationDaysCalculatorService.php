254d2dae25dd83a8f73631d70e4d7195f2c26838
Mahmoud Abdelsattar <jinkazama_m@yahoo.com>
2021-07-04 16:17:31 +0200

    Creating VacationDaysCalculatorService.php

---
+++ src/Ottivo/Service/VacationDaysCalculatorService.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace VacationCalculator\Ottivo\Service;
+
+class VacationDaysCalculatorService
+{
+
+    public function calculateDays()
+    {
+        echo 'Hii';
+    }
+}
--- temp.php
+++ temp.php
@@ -1,4 +1,11 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
+use VacationCalculator\Ottivo\Service\VacationDaysCalculatorService;
+
+$vacationDaysCalculatorService = new VacationDaysCalculatorService();
+$vacationDaysCalculatorService->calculateDays();
+
 echo '<h2>Just for development output purpose</h2>';
 exit;
